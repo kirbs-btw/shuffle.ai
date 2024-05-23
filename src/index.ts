029b3088fefb0e8e5c200dd5f9ca888fc7878233
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2024-05-23 18:19:38 +0200

    testing the data posting with api

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-// generic api setup with express
+// basic api setup
 
 const app = express();
 const port = 3000;
@@ -11,9 +11,22 @@ app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
+app.get('/ping', (req, res) => {
+  // i can only return one response to the client
+  // but i can execute code after the return
+  res.send("fff");
+  console.log("hallo das ist cool!");
+
+});
+
+
 app.post('/data', (req, res) => {
+  // the response of the server is still not reachable 
+  // something with the setup of the server side script 
+
   const data = req.body;
-  res.send(`You sent: ${JSON.stringify(data)}`);
+  res.send(`You sent: ${data}`);
+  // res.send(`You sent: ${JSON.stringify(data)}`);
 });
 
 app.listen(port, () => {
