537e283cfa3bc16e3ad5367aee620cc6ca573b8b
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2024-06-10 23:24:40 +0200

    Create Playlist class

---
+++ src/Playlist.ts
@@ -0,0 +1,17 @@
+class Playlist{
+   private songs: string[];
+   private dir: float[]; // to be changed 
+
+
+
+  function calcDir(): float[]{}
+  function addSong(song: Song){
+
+
+  }
+
+  function delSong(id: string){
+    
+  }
+
+}
