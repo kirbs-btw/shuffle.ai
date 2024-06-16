d907bce9f09e90141ce235b1071e6333b279d605
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2024-06-16 23:50:28 +0200

    search explanation

--- src/search.ts
+++ src/search.ts
@@ -1,3 +1,9 @@
-function search(userinput: string){
-    // searching the db for the searched song
-}
+import { Song } from "./Song.js";
+
+// executed with every typing in the keyboard from the searchbar
+// one change
+function search(userinput: string): Song[]{
+    // searching database as a stream
+
+    return null;
+}   
