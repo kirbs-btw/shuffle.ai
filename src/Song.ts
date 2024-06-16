37c2c7dc834bdb8e17390aecaf7acde43710ee3c
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2024-06-16 23:49:18 +0200

    extendet song class

--- src/Song.ts
+++ src/Song.ts
@@ -3,28 +3,46 @@ export class Song{
     private name: string;
     private artist: string;
     private duration: number;
+    private link: string;
     private vector: number[];
 
-
-    constructor(name:string, artist:string, duration:number){
+    constructor(name:string, artist:string, duration:number, link:string){
         this.name = name;
         this.artist = artist;
         this.duration = duration;
+        this.link = link; // on our site or just web idk
+
+        this.initVector();
     }
 
-    public getName():string {
+    public initVector(): number[] {
+        // getting the v with the db i guess
+    
+        return this.vector;
+    }
+
+    public getName(): string {
         return this.name;
     }   
 
-    public getArtist():string {
+    public getArtist(): string {
         return this.artist;
     }
 
-    public getDuration():number {
+    public getDuration(): number {
         return this.duration;
     }
 
-    public getVector():number[] {
+    public setLink(newLink:string): void {
+        // the link to the song can change
+        this.link = newLink;
+    }
+
+    public getLink(): string {
+        return this.link;
+    }
+
+    public getVector(): number[] {
         return this.vector;
     }
 }
