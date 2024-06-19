4764e9140ae64fd32fce85836ec9a479f3a42086
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2024-06-19 22:26:42 +0200

    Delete src/search.ts

--- src/search.ts
+++
@@ -1,9 +0,0 @@
-import { Song } from "./Song.js";
-
-// executed with every typing in the keyboard from the searchbar
-// one change
-function search(userinput: string): Song[]{
-    // searching database as a stream
-    let result = db.filter(item => item.includes(userinput));
-    return result;
-}   
