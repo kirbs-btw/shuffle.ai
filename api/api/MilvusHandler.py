efc5ceb6f0c28f83391354fc7653663937e5f1d4
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2024-07-05 17:07:33 +0200

    Milvus connection

--- api/api/MilvusHandler.py
+++ api/api/MilvusHandler.py
@@ -1,11 +1,26 @@
 import numpy as np
-
+from pymilvus import (
+    connections,
+    utility,
+    FieldSchema, 
+    CollectionSchema, 
+    DataType,
+    Collection,
+)
+from sentence_transformers import SentenceTransformer 
 
 class MilvusHandler:
-    def __init__(self):
-        pass
-    
-    def __avg_vector_array(self, vectorArr):
+    def __init__(self, fields, collectionName = "SONG_DB", dim=384, connection_type:str = "default", host:str="localhost", port:str="19530"):
+        self.collection_name:str = collectionName
+        self.dim:int = dim
+        
+        # connect to db
+        self.__connect_to_db(connection_type=connection_type, host=host, port=port)
+        
+    def __connect_to_db(self, connection_type:str = "default", host:str="localhost", port:str="19530") -> None:
+        connections.connect(connection_type, host=host, port=port)
+        
+    def __avg_vector_array(self, vectorArr) -> np.array:
         # Convert vectors to numpy arrays
         npVectorArr = [np.array(i) for i in vectorArr]
         
@@ -18,4 +33,14 @@ class MilvusHandler:
         sumV = npVectorArr[0]
         for i in npVectorArr[1:]: sumV += i
         return sumV / len(vectorArr)
+    
+    def ingest(self) -> Collection:
+        fields = [
+            FieldSchema(name="id", dtype=DataType.INT64, is_primary=True, auto_id=True),
+            FieldSchema(name="embeddings", dtype=DataType.FLOAT_VECTOR, dim=self.dim)
+        ]
+        
+        schema = CollectionSchema(fields, description="")
+        collection_milvus = Collection(version_name=self.collection_name, schema=schema, consistency_level="Strong")
+        return collection_milvus
     
