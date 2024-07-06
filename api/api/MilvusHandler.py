5ff18992be83e73c6bec2490b18da496286dd205
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2024-07-06 12:33:21 +0200

    Add files via upload

--- api/api/MilvusHandler.py
+++ api/api/MilvusHandler.py
@@ -1,7 +1,6 @@
 import numpy as np
 from pymilvus import (
     connections,
-    utility,
     FieldSchema, 
     CollectionSchema, 
     DataType,
@@ -10,11 +9,19 @@ from pymilvus import (
 from sentence_transformers import SentenceTransformer 
 
 class MilvusHandler:
-    def __init__(self, fields, collectionName = "SONG_DB", dim=384, connection_type:str = "default", host:str="localhost", port:str="19530"):
+    def __init__(self, 
+                 fields, 
+                 collectionName = "SONG_DB", 
+                 indexName = "embeddings",
+                 dim=384, 
+                 connection_type:str = "default", 
+                 host:str="localhost", 
+                 port:str="19530"):
+        
         self.collection_name:str = collectionName
         self.dim:int = dim
+        self.index_name:str = indexName
         
-        # connect to db
         self.__connect_to_db(connection_type=connection_type, host=host, port=port)
         
     def __connect_to_db(self, connection_type:str = "default", host:str="localhost", port:str="19530") -> None:
@@ -34,7 +41,7 @@ class MilvusHandler:
         for i in npVectorArr[1:]: sumV += i
         return sumV / len(vectorArr)
     
-    def ingest(self) -> Collection:
+    def setup_collection(self) -> Collection:
         fields = [
             FieldSchema(name="id", dtype=DataType.INT64, is_primary=True, auto_id=True),
             FieldSchema(name="embeddings", dtype=DataType.FLOAT_VECTOR, dim=self.dim)
@@ -44,3 +51,32 @@ class MilvusHandler:
         collection_milvus = Collection(version_name=self.collection_name, schema=schema, consistency_level="Strong")
         return collection_milvus
     
+    def ingest_data(self, data):
+        """ 
+        data = [
+            [id, song_description] 
+        ]
+        """
+        
+        id_arr: list = [data_point[0] for data_point in data]
+        description_arr: list = [data_point[1] for data_point in data]
+        
+        model: SentenceTransformer = SentenceTransformer('all-MiniLM-L6-v2')    
+
+        description_embedding: list = model.encode(description_arr)
+        
+        entities = [
+            id_arr, 
+            description_embedding,
+        ]
+
+        collection_milvus: Collection = Collection(name=self.collection_name) 
+        # insert data to milvus
+        collection_milvus.insert(entities)
+        
+        index = {
+            "index_type": "IVF_FLAT",
+            "metric_type": "L2",
+            "params": {"nlist": 128},
+        }
+        collection_milvus.create_index(self.index_name, index)
