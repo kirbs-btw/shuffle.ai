71fa6c680bf09ce546656b23e3d77bf377cb6b6f
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2024-07-15 23:16:51 +0200

    adding some querys (no db conn)

--- relational_db_api/api/DbHandler.py
+++ relational_db_api/api/DbHandler.py
@@ -3,15 +3,27 @@ class DbHandler:
         pass
     
     def search_input(self, search_input:str) -> list:
+        results: dict = {
+            "some spec how the dict will look idk"
+        }
         # checking for sql inject
-        
+        search_lower: str = search_input.lower()
+
         # search for complete input artist and title
-        query = "FROM songs SELECT * WHERE title == {search_input}"
-        query = "FROM songs SELECT * WHERE artist == {search_input}"
+        query: str = "SELECT * FROM songs WHERE title LIKE '%{}%';".format(search_lower) # rank spot #0
+        query: str = "SELECT * FROM songs WHERE artist LIKE '%{}%';".format(search_lower) # rank #1
 
         # search for parts of the input ignore middle words like ("to", "the", "a", ...)
+        words: list = search_lower.split()
+        for word in words:
+            query: str = "SELECT * FROM songs WHERE title LIKE '%{}%';".format(word) # rank spot #2
+            query: str = "SELECT * FROM songs WHERE artist LIKE '%{}%';".format(word) # rank #3        
+            # results.append("query results")
+
+        # maybe combine the querys to one big one to have faster search ?
+
 
         # ranking search results
         # contains  id, title, link, all other data from that db...
-        results: dict
-        return results 
+        
+        return results   
