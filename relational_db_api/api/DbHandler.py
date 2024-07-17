09d188f7d5f7dade89abfe93a0a4c5f0788c2cb0
Bastian Lipka <lipka.bastian04@gmail.com>
2024-07-17 23:19:28 +0200

    dbhandler update

--- relational_db_api/api/DbHandler.py
+++ relational_db_api/api/DbHandler.py
@@ -14,18 +14,18 @@ class DbHandler:
         # search for complete input artist and title
         query: str = "SELECT * FROM songs WHERE title LIKE '%{}%';".format(search_lower) # rank spot #0
         query: str = "SELECT * FROM songs WHERE artist LIKE '%{}%';".format(search_lower) # rank #1
-        results["songs"].append("sth")  
+        results["songs"].append(self.__query_db(query))  
 
         # search for parts of the input ignore middle words like ("to", "the", "a", ...)
         words: list = search_lower.split()
+
         for word in words:
             query: str = "SELECT * FROM songs WHERE title LIKE '%{}%';".format(word) # rank spot #2
             query: str = "SELECT * FROM songs WHERE artist LIKE '%{}%';".format(word) # rank #3      
-            results["songs"].append("sth")  
-            # results.append("query results")
+            results["songs"].append(self.__query_db(query))  
+
 
         # maybe combine the querys to one big one to have faster search ?
-
 
         # ranking search results
         # contains  id, title, link, all other data from that db...
@@ -33,3 +33,9 @@ class DbHandler:
         # send ping to a other db api that count the searched times for tracking
 
         return results   
+    
+    def __query_db(self, search_query):
+        results = []
+
+
+        return results
