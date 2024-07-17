66c52e151cba41d91cbf85e9b025f533eb409057
Bastian Lipka <lipka.bastian04@gmail.com>
2024-07-18 00:17:46 +0200

    debugging api

--- vector_db_api/setup.sh
+++ vector_db_api/setup.sh
@@ -3,4 +3,3 @@ cd milvus_compose
 wget https://github.com/milvus-io/milvus/releases/download/v2.2.8/milvus-standalone-docker-compose.yml -O docker-compose.yml
 docker compose up -d
 docker ps -a
-pip3 install pymilvus==2.2
