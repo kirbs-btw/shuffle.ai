81a0c8971e894d5e909d2a48ebb0a31989e37422
Bastian Lipka <lipka.bastian04@gmail.com>
2024-08-02 00:01:14 +0200

    debugged mivlus api

--- vector_db_api/api/MilvusHandler.py
+++ vector_db_api/api/MilvusHandler.py
@@ -69,6 +69,7 @@ class MilvusHandler:
         return self.search_embedding(playlist_embedding)
 
     def setup_collection(self) -> Collection:
+        # todo debug this
         fields:list = [
             FieldSchema(name="id", dtype=DataType.INT64, is_primary=True, auto_id=True),
             FieldSchema(name="embedding", dtype=DataType.FLOAT_VECTOR, dim=self.dim)
--- vector_db_api/api/api.py
+++ vector_db_api/api/api.py
@@ -27,15 +27,9 @@ def search_word():
     # don't know if in need ?
     pass
 
-# curl -X POST -H "Content-Type: application/json" -d '{ "song_ids": [ {"id": "d6416719-d5ad-44f9-ba72-7a922865bc3b"}, {"id": "5f3c97c1-cc14-4429-8e62-c586106ea7d3"}]}' http://127.0.0.1:5000/search/playlist
-
-# curl -X POST -H "Content-Type: application/json" -d "{}" http://127.0.0.1:5000/search/playlist
-
-# curl -X POST -H "Content-Type: application/json" -d """{ "playlist": [{"id": "d6416719-d5ad-44f9-ba72-7a922865bc3b"}, {"id": "5f3c97c1-cc14-4429-8e62-c586106ea7d3"}]}""" http://127.0.0.1:5000/search/playlist
-
-
 @api.post('/search/playlist')
 def search_playlist():
+    # need to optimize that fun stuff here
     """
     playlist has form 
     {
@@ -62,7 +56,7 @@ def search_playlist():
     }
     for hits in results:
        for hit in hits:
-           response["song_ids"].append(hit["id"])
-
+           response["song_ids"].append(hit.id)
+    
     return response
     
